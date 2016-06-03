c49e613c7e00627791914515295e3cb65655c2fe
William Goulart <williamgoulart1@hotmail.com>
2016-06-03 06:10:10 -0300

    update sass and code.js

--- scripts/code.js
+++ scripts/code.js
@@ -1,18 +1,24 @@
-var url = "http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=jsonp&lang=en&jsonp=?"
+// Random Quote Generator
+var url = "http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=jsonp&lang=en&jsonp=?";
 
-$(document).ready(function() {
-  function getQuote(jsonReturn) {
-    if ((jsonReturn !== null) && ((jsonReturn.quoteAuthor !== "") || (jsonReturn.quoteAuthor !== ""))) {
-      $(".quote-text").text(jsonReturn.quoteText);
-      $(".random-quote-author").text(jsonReturn.quoteAuthor);
-    } else {
-      $('.random-quote-text').html("No such quote's ");
-    }
+var getQuote = function (jsonReturn) {
+  $(".quote-text").text(jsonReturn.quoteText);
+
+  // check author quote
+  if (jsonReturn.quoteAuthor === '') {
+    jsonReturn.quoteAuthor = 'Unknown';
   }
+  $(".random-quote-author").text(jsonReturn.quoteAuthor);
 
-  $(".random-quote-button").on('click', function () {
-    $.getJSON(url, getQuote, 'jsonp');
-  });
+  // share-twitter
+  var shareQuote = 'https://twitter.com/intent/tweet?text=' + jsonReturn.quoteText + ' - ' + jsonReturn.quoteAuthor + ' via @wgoulaart';
+  $(".share-tt").attr("href", shareQuote);
+};
 
+$(document).ready(function () {
+  $.getJSON(url, getQuote, 'jsonp');
+});
+
+$(".random-quote-button").on('click', function () {
   $.getJSON(url, getQuote, 'jsonp');
 });
